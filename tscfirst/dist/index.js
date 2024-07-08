eda0edeb3ccd089c2e8ec6ffde0f6254f28da295
babubhare@gmail.com <babubhare@gmail.com>
2024-07-08 10:30:35 +0000

    new files

---
+++ tscfirst/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('types cript');
+let a = 100;
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ tscfirst/src/index.ts
@@ -0,0 +1,6 @@
+console.log('types cript');
+
+let a: number = 100;
+
+
+console.log(a);
